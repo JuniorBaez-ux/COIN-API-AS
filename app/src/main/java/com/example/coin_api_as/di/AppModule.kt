48e63740e03f070e1ceda251ac5dec74660b05c7
Junior Baez <junior_baez@ucne.edu.do>
2022-06-24 21:17:47 -0400

    Creando AppModule del proyecto

---
+++ app/src/main/java/com/example/coin_api_as/di/AppModule.kt
@@ -0,0 +1,43 @@
+package com.example.coin_api_as.di
+
+import com.example.coin_api_as.data.remote.CoinApi
+import com.example.coin_api_as.repository.CoinRepository
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi {
+        return Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideCoinApi(moshi: Moshi): CoinApi {
+        return Retrofit.Builder()
+            .baseUrl("http://www.JuniorBaez.somee.com")
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(CoinApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideCoinRepository(coinApi: CoinApi): CoinRepository {
+        return CoinRepository(coinApi)
+    }
+}
+
