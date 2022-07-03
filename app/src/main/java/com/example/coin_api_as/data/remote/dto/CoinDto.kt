c2f8c4debd876dae6aa4d231ea933551c73c401f
Junior Baez <junior_baez@ucne.edu.do>
2022-07-02 23:06:25 -0400

    Fix Typo

--- app/src/main/java/com/example/coin_api_as/data/remote/dto/CoinDto.kt
+++ app/src/main/java/com/example/coin_api_as/data/remote/dto/CoinDto.kt
@@ -1,7 +1,7 @@
 package com.example.coin_api_as.data.remote.dto
 
 data class CoinDto(
-    val monedaId: Int,
+    val monedaId: Int?,
     val descripcion: String ?,
     val valor: Double ?,
     val imageUrl: String?
