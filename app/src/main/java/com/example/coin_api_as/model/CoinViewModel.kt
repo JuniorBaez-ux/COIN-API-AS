9d7562ff7153fa11d7fee3f9f9ba13ed093fac1c
Junior Baez <junior_baez@ucne.edu.do>
2022-07-02 23:07:41 -0400

    Conectando POST del repository con el viewModel e utilizandolo

--- app/src/main/java/com/example/coin_api_as/model/CoinViewModel.kt
+++ app/src/main/java/com/example/coin_api_as/model/CoinViewModel.kt
@@ -1,26 +1,47 @@
 package com.example.coin_api_as.model
 
 import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.coin_api_as.data.CoinListState
+import com.example.coin_api_as.data.remote.dto.CoinDto
 import com.example.coin_api_as.repository.CoinRepository
 import com.example.coin_api_as.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+import retrofit2.Response
 import javax.inject.Inject
 
 @HiltViewModel
 class CoinViewModel @Inject constructor(
     private val coinRepository: CoinRepository
 ) : ViewModel() {
-
+    var monedaId by mutableStateOf(null)
+    var descripcion by mutableStateOf("")
+    var valor by mutableStateOf("")
+    var imageUrl by mutableStateOf("")
     private var _state = mutableStateOf(CoinListState())
     val state: State<CoinListState> = _state
+    var myResponse: MutableLiveData<Response<CoinDto>> = MutableLiveData()
 
     init {
+        obtainCoins()
+    }
+
+    fun makePost(post: CoinDto){
+        viewModelScope.launch {
+            val response: Response<CoinDto> = coinRepository.makePost(post)
+            myResponse.value = response
+        }
+    }
+
+    fun obtainCoins(){
         coinRepository.getExchanges().onEach { result ->
             when (result) {
                 is Resource.Loading -> {
@@ -36,5 +57,4 @@ class CoinViewModel @Inject constructor(
             }
         }.launchIn(viewModelScope)
     }
-
 }
